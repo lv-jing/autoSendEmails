9103d8332ca867493d5e653d9c4411a4e21571b8
jinglv <lvjing520cxc>
2021-10-09 16:39:45 +0800

    第一次

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '大憨憨', // 收件箱展示的来件人名字
+  fromDisplaySubText: '爱情记录', // 收件箱展示的次级标题
+  user: '732796330@qq.com', // 发送者邮箱
+  pass: 'rowfoubxzkmjbgah', // 发送者邮箱MTP协议密码
+  to: '732796330@qq.com', // 发送到谁，填邮箱
+  weatherKey: '05182e54775a40e883141fdec7398705', // 和风天气key
+  location: '101040100',  // 和风天气-成都武侯区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: '91cd74fd5375315a578ecbdc0ab535bc', // 天行数据的key
+  startDay: '2015-05-19', // 在一起的日期
+}
